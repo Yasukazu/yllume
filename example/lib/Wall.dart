d1e9a4b6fe2e4ec29ab31bfb0b66a5f91c944954
Yasukazu Makino <mkn@yskz.dev>
2022-05-07 08:08:18 +0900

    Ball member

--- example/lib/Wall.dart
+++ example/lib/Wall.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:example/orgMain.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:tuple/tuple.dart';
-import 'orgMain.dart';
 import 'MyHomePage.dart';
+import 'Ball.dart';
 import 'package:illume/illume.dart';
 import 'package:intl/intl.dart';
 
@@ -16,10 +14,10 @@ typedef Vector2 v2ToV2(Vector2 vector2);
 class WallO extends GameObject {
   static const shape = BoxShape.rectangle;
   static const b = MyHomePage.wallT;
-  static topOffset(Vector2 gameSize) => Vector2(gameSize[0] / 2, b / 2);
-  static leftOffset(Vector2 gameSize) => Vector2(b / 2, gameSize[1] / 2);
-  static bottomOffset(Vector2 gameSize) => Vector2(gameSize[0] / 2, gameSize[1] - b / 2);
-  static rightOffset(Vector2 gameSize) => Vector2(gameSize[0] - b / 2, gameSize[1] / 2);
+  static topOffset(Vector2 gameSize) => Vector2(gameSize[0] / 2, b / 2 * gameSize[1]);
+  static leftOffset(Vector2 gameSize) => Vector2(b / 2 * gameSize[0], gameSize[1] / 2);
+  static bottomOffset(Vector2 gameSize) => Vector2(gameSize[0] / 2, gameSize[1] * ( 1 - b / 2));
+  static rightOffset(Vector2 gameSize) => Vector2(gameSize[0] * (1 - b / 2), gameSize[1] / 2);
   static const offsets = [topOffset, leftOffset, bottomOffset, rightOffset];
   double get x => offset[0];
   double get y => offset[1];
@@ -42,8 +40,9 @@ class WallO extends GameObject {
     }
   }
   final wallPos pos;
+  final BallO ball;
 
-  WallO(this.pos);
+  WallO(this.pos, this.ball);
 
   @override
   void init() {
@@ -77,20 +76,22 @@ class WallO extends GameObject {
 
   @override
   void onCollision(List<Collision> collisions) {
-    final fmt = NumberFormat('##.0#', 'en_US');
-    final lenText = "Wall .len=${collisions.length} at ballX=${fmt.format(MyHomePage.ballX)},ballY=${fmt.format(MyHomePage.ballY)}";
+    // final fmt = NumberFormat('##.0#', 'en_US');
+    // final lenText = "Wall .len=${collisions.length} at ballX=${fmt.format(MyHomePage.ballX)},ballY=${fmt.format(MyHomePage.ballY)}";
+    assert(collisions.length == 1); // only ball
+    assert(collisions[0].component == ball);
+    ball.backward();
+    logger.info("ball backward by wall $pos.");
     switch(pos) {
       case wallPos.left:
       case wallPos.right:
-        MyHomePage.ballDxRev = !MyHomePage.ballDxRev;
-        MyHomePage.wallMsg =
-        "$lenText:ballDxRev is reverted to: ${MyHomePage.ballDxRev}";
+        ball.dxReverse = !ball.dxReverse;
+        logger.info("ball dxReverse is reversed to ${ball.dxReverse}.");
         return;
       case wallPos.top:
       case wallPos.bottom:
-        MyHomePage.ballDxRev = !MyHomePage.ballDxRev;
-        MyHomePage.wallMsg =
-        "$lenText:ballDyRev is reverted to: ${MyHomePage.ballDyRev}";
+        ball.dyReverse = !ball.dyReverse;
+        logger.info("ball dyReverse is reversed to ${ball.dyReverse}");
         return;
     }
   }
