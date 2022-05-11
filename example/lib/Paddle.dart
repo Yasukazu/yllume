7ea960d1e4634f9e6468b3b0b06ef992c438cb8e
Yasukazu Makino <mkn@yskz.dev>
2022-05-12 05:40:01 +0900

    Paddle design

--- example/lib/Paddle.dart
+++ example/lib/Paddle.dart
@@ -11,19 +11,20 @@ class PaddleO extends HWallO {
   PaddleO(wallPos pos) : super(pos) {
     assert(pos == wallPos.top || pos == wallPos.bottom);
   }
-  static const gapToWall = 0.05;
-  static const b = 0.25;
+  static const gapToWall = 0.02;
+  static const b = 0.1;
   @override
   Color getColor() => Colors.yellow;
 
   @override
   Vector2 getOffset() {
-    final baseOffset = super.offset;
+    final baseOffset = super.getOffset();
     return pos == wallPos.bottom
-        ? Vector2(baseOffset[0], (1 - WallO.b - b - gapToWall) * gameSize[1])
+        ? Vector2(baseOffset[0], (1 - WallO.b - b/2 - gapToWall) * gameSize[1])
         : Vector2(baseOffset[0], (WallO.b + gapToWall + b / 2) * gameSize[1]);
   }
 
   @override
   Vector2 getRect() => Vector2(wRatio * gameSize[0], b * gameSize[1]);
 }
+
--- example/lib/orgMain.dart
+++ example/lib/orgMain.dart
@@ -9,7 +9,6 @@ late final MyApp myApp;
 
 void main() {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
-  logger.level = Level.INFO;
   Logger.root.onRecord.listen((record) {
     print('${record.level.name}: ${record.time}: ${record.message}');
   });
