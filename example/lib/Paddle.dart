74dac3dfa3bfdc1412cfaddd63e3e363b6b3e4b8
Yasukazu Makino <mkn@yskz.dev>
2022-05-11 14:10:54 +0900

    new

---
+++ example/lib/Paddle.dart
@@ -0,0 +1,22 @@
+import 'package:example/orgMain.dart';
+import 'package:flutter/material.dart';
+import 'MyHomePage.dart';
+import 'Ball.dart';
+import 'Wall.dart';
+import 'package:illume/illume.dart';
+import 'package:intl/intl.dart';
+
+class PaddleO extends HWallO {
+  PaddleO(wallPos pos) : super(pos);
+
+  static const gapToWall = 5;
+  static const b = 12;
+
+  @override
+  Vector2 getOffset() {
+    final baseOffset = super.offset;
+    return pos == wallPos.bottom ? Vector2(baseOffset[0], baseOffset[1] - b - gapToWall):
+    Vector2(baseOffset[0], (WallO.b + gapToWall + b / 2) * gameSize[1]);
+  }
+
+}
