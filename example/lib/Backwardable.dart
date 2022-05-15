6c1403c7b12f7bfa9a123c576cb1a1e4170413c6
Yasukazu Makino <mkn@yskz.dev>
2022-05-15 11:16:36 +0900

    lastPosForBackward null initialize removed.

--- example/lib/Backwardable.dart
+++ example/lib/Backwardable.dart
@@ -1,16 +1,11 @@
-import 'package:flutter/material.dart';
-import 'dart:math';
-import 'orgMain.dart'; // logger
-import 'MyHomePage.dart'; // WallO
-import 'WallBase.dart';
 import 'package:illume/illume.dart';
-import 'package:intl/intl.dart';
 
 mixin Backwardable on GameObject {
   Vector2? lastPosForBackward;
   void backward(GameObject go) {
     if (lastPosForBackward != null) {
       go.position = lastPosForBackward as Vector2;
+      lastPosForBackward = null;
     }
   }
 }
