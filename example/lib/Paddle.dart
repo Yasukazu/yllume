568d8e9b3d071b3f288f987eb68885cf3fd0870f
Yasukazu Makino <mkn@yskz.dev>
2022-05-15 11:18:50 +0900

    step, RangeNum

--- example/lib/Paddle.dart
+++ example/lib/Paddle.dart
@@ -1,28 +1,68 @@
+import 'package:example/orgMain.dart';
 import 'package:flutter/material.dart';
 import 'Wall.dart';
 import 'WallBase.dart';
+import 'Backwardable.dart';
 import 'package:illume/illume.dart';
 
-class PaddleO extends WallO {
-  static const wRatio = 0.2;
-  PaddleO(wallPos pos) : super(pos) {
+class PaddleO extends WallO with Backwardable {
+  late final double step;
+  late final double width;
+  late final RangeNum rn;
+  @override
+  double get x => rn.d + offset[0];
+  PaddleO(wallPos pos, this.width, this.step) : super(pos) {
     assert(pos == wallPos.top || pos == wallPos.bottom);
+    assert(width > 0 && width <= 1);
+    assert(step > 0 && step <= 1);
+    rn = RangeNum(1 - width);
   }
+
   static const gapToWall = 0.02;
   static const b = 0.1;
+  late final RangeNum range;
 
   @override
   Color getColor() => Colors.yellow;
 
   @override
-  Vector2 getOffset() {
-    final baseOffset = super.getOffset();
-    return pos == wallPos.bottom
-        ? Vector2(
-            baseOffset[0], (1 - WallO.b - b / 2 - gapToWall) * gameSize[1])
-        : Vector2(baseOffset[0], (WallO.b + gapToWall + b / 2) * gameSize[1]);
+  Vector2 getRect() => Vector2(width * gameSize[0], b * gameSize[1]);
+
+  void moveRight() {
+    if (lastPosForBackward != null) {
+      return;
+    }
+    lastPosForBackward = position;
+    logger.finer("move Right");
+    rn.inc(step);
   }
 
-  @override
-  Vector2 getRect() => Vector2(wRatio * gameSize[0], b * gameSize[1]);
+  void moveLeft() {
+    if (lastPosForBackward != null) {
+      return;
+    }
+    lastPosForBackward = position;
+    logger.finer("move Left");
+    rn.dec(step);
+  }
+}
+
+class RangeNum {
+  double _d = 0;
+  double get d => _d;
+  final double range;
+
+  RangeNum(this.range);
+
+  void inc(double step) {
+    if ((_d + step) <= range / 2) {
+      _d += step;
+    }
+  }
+
+  void dec(double step) {
+    if ((_d - step) >= -range / 2) {
+      _d -= step;
+    }
+  }
 }
