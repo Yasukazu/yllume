a3bffdc7a992377ebe0c8b31b09921903acf95d6
Yasukazu Makino <mkn@yskz.dev>
2022-05-27 23:49:30 +0900

    PaddleO extends GameObject

--- example/lib/Paddle.dart
+++ example/lib/Paddle.dart
@@ -7,35 +7,25 @@ import 'WallBase.dart';
 import 'Backwardable.dart';
 import 'package:illume/illume.dart';
 
-class PaddleO extends WallBaseO with Backwardable {
+class PaddleO extends GameObject with Backwardable {
+  static const color = Colors.yellow;
+  static const shape = BoxShape.rectangle;
   late final double step;
   late final double width;
   late final RangeNum rn;
   static const wallGapRatio = MyHomePage.ballSize / 2;
   late final double wallGap;
   double _x = 0;
-  @override
   double get x => _x;
-  late final _dx;
+  late final double _dx;
   double get dx => _dx;
   late final double _y;
-  @override
-  double get y => _y;
-  /*
-  final _offset = Vector2(0, 0);
-  @override
-  Vector2 get offset => _offset;
-  */
-  @override
-  Vector2 getOffset() {
-    return Vector2(offset[0], offset[1] + rdiff);
-  }
+  final wallPos pos;
 
-  PaddleO(wallPos pos, this.width, this.step) : super(pos) {
+  PaddleO(this.pos, this.width, this.step) {
     assert(pos == wallPos.top || pos == wallPos.bottom);
     assert(width > 0 && width <= 1);
     assert(step > 0 && step <= 1);
-    rn = RangeNum(1 - width);
   }
 
   static const b = MyHomePage.paddleT;
@@ -43,7 +33,6 @@ class PaddleO extends WallBaseO with Backwardable {
 
   @override
   void init() {
-    super.init();
     // _offset[0] = super.offset[0];
     final diff =
         (MyHomePage.wpGap + MyHomePage.wallT / 2 + b / 2) * gameSize[1];
@@ -52,36 +41,54 @@ class PaddleO extends WallBaseO with Backwardable {
         : WallBaseO.bottomOffset(gameSize) - diff;
     _x = gameSize[0] / 2;
     _dx = MyHomePage.paddleStep * gameSize[0];
+    rn = RangeNum((1 - width) * gameSize[0]);
   }
 
   @override
   void update(Duration delta) {
-    super.update(delta);
     // x = rn.d + offset[0];
     position[0] = x;
     logger.finer("Paddle.x = $x; position[0]= ${position[0]}");
   }
 
   @override
-  Color getColor() => Colors.yellow;
-
-  @override
-  Vector2 getRect() => Vector2(width * gameSize[0], b * gameSize[1]);
-
-  @override
   void onCollision(List<Collision> collisions) {
     final col = collisions[0];
     if (col is WallO) {
       backward(this);
-      _x = lastPosForBackward[0];
-      logger.finer("Paddle backward with Wall.");
+      if (lastPosForBackward != null) {
+        _x = (lastPosForBackward as Vector2)[0];
+        logger.finer("Paddle backward with Wall.");
+      }
     }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return
+        // Container( //alignment: Alignment(x, y), child:
+        Stack(alignment: AlignmentDirectional.center, children: [
+      Container(
+        decoration: const BoxDecoration(shape: shape, color: color),
+        // width: size[0],
+        // height: size[1],
+      ),
+      Container(
+        decoration: const BoxDecoration(shape: shape, color: Colors.black),
+        width: 100,
+        height: 100,
+      ),
+    ]);
+  }
+
+  @override
+  void onScreenSizeChange(Vector2 size) {}
 
   void moveRight() {
     // if (lastPosForBackward != null) { return; }
     lastPosForBackward = position;
-    _x += dx;
+    rn.inc(dx);
+    _x = rn.d;
     // rn.inc(step);
     logger.finer("move Right. x = $x");
   }
@@ -89,8 +96,8 @@ class PaddleO extends WallBaseO with Backwardable {
   void moveLeft() {
     // if (lastPosForBackward != null) { return; }
     lastPosForBackward = position;
-    _x -= dx;
-    // rn.dec(step);
+    rn.dec(dx);
+    _x = rn.d;
     logger.finer("move Left. x = $x");
   }
 }
@@ -110,12 +117,16 @@ class RangeNum {
   void inc(double step) {
     if ((_d + step) <= range / 2) {
       _d += step;
+    } else {
+      _d = range / 2;
     }
   }
 
   void dec(double step) {
     if ((_d - step) >= -range / 2) {
       _d -= step;
+    } else {
+      _d = -range / 2;
     }
   }
 }
