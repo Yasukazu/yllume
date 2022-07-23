3249dc4128ca5b47404267d925cdc27530939be4
Yasukazu Makino <mkn@yskz.dev>
2022-07-23 10:06:53 +0900

    _setSteps to make _dx _dy change valid.

--- example/lib/Ball.dart
+++ example/lib/Ball.dart
@@ -116,273 +116,291 @@ class BallO extends GameObject with Backwardable {
     return sqrt(x_ * x_ + y_ * y_); // outer size
   }
 
-  @override
-  void init() {
-    final gx = gameSize[0];
-    final gy = gameSize[1];
-    final diagonal = sqrt(gx * gx + gy * gy);
-    final stepLength = diagonal / defaultBallFPS * speed / 1000;
-    stepInterval = diagonal / stepLength;
-    _stepX = stepLength * dx;
-    _stepY = stepLength * dy;
-    logger.finer("stepInterval = $stepInterval");
-    // final virtualLandingPoint = y * dx / dy;
-    final oSize = calcSize(gameSize, ratio);
-    iSize = oSize * iRatio;
-    logger.finer("Ball outer size = $oSize");
-    size = Vector2.all(oSize);
-    alignment = GameObjectAlignment.center;
-    // final double offset = WallBaseO.bottomOffset(gameSize)[1];
-    final double selfPaddleYPos = selfPaddle.position[1];
-    final double selfPaddleHSize = selfPaddle.size[1];
-    final double selfPaddleTopSurface = selfPaddleYPos - selfPaddleHSize / 2;
-    position =
-        Vector2(gx / 2, selfPaddleTopSurface - oSize / 2 - gap); // , gy / 2);
-    initialised = true;
-    _stepCount = 0;
-    rebuildWidgetIfNeeded = true;
-    randSignIterator.moveNext();
-    motionLine.givenSize = size;
-  }
+    void _setSteps() {
+      final gx = gameSize[0];
+      final gy = gameSize[1];
+      final diagonal = sqrt(gx * gx + gy * gy);
+      final stepLength = diagonal / defaultBallFPS * speed / 1000;
+      _stepX = stepLength * dx;
+      _stepY = stepLength * dy;
+    }
 
-  @override
-  void onScreenSizeChange(Vector2 size) {
-    final gameSize = size;
-    final gx = gameSize[0];
-    final gy = gameSize[1];
-    final diagonal = sqrt(gx * gx + gy * gy);
-    final stepLength = diagonal / defaultBallFPS * speed / 1000;
-    stepInterval = diagonal / stepLength;
-    _stepX = stepLength * dx;
-    _stepY = stepLength * dy;
-    logger.finer("stepInterval = $stepInterval");
-    final oSize = calcSize(gameSize, ratio);
-    iSize = oSize * iRatio;
-    logger.finer("Ball outer size = $oSize");
-    this.size = Vector2.all(oSize);
-    alignment = GameObjectAlignment.center;
-    final double selfPaddleYPos = selfPaddle.position[1];
-    final double selfPaddleHSize = selfPaddle.size[1];
-    final double selfPaddleTopSurface = selfPaddleYPos - selfPaddleHSize / 2;
-    position =
-        Vector2(gx / 2, selfPaddleTopSurface - oSize / 2 - gap); // , gy / 2);
-    initialised = true;
-    _stepCount = 0;
-    randSignIterator.moveNext();
-  }
+    @override
+    void init() {
+      final gx = gameSize[0];
+      final gy = gameSize[1];
+      final diagonal = sqrt(gx * gx + gy * gy);
+      final stepLength = diagonal / defaultBallFPS * speed / 1000;
+      stepInterval = diagonal / stepLength;
+      _stepX = stepLength * dx;
+      _stepY = stepLength * dy;
+      logger.finer("stepInterval = $stepInterval");
+      // final virtualLandingPoint = y * dx / dy;
+      final oSize = calcSize(gameSize, ratio);
+      iSize = oSize * iRatio;
+      logger.finer("Ball outer size = $oSize");
+      size = Vector2.all(oSize);
+      alignment = GameObjectAlignment.center;
+      // final double offset = WallBaseO.bottomOffset(gameSize)[1];
+      final double selfPaddleYPos = selfPaddle.position[1];
+      final double selfPaddleHSize = selfPaddle.size[1];
+      final double selfPaddleTopSurface = selfPaddleYPos - selfPaddleHSize / 2;
+      position =
+          Vector2(gx / 2, selfPaddleTopSurface - oSize / 2 - gap); // , gy / 2);
+      initialised = true;
+      _stepCount = 0;
+      rebuildWidgetIfNeeded = true;
+      randSignIterator.moveNext();
+      motionLine.givenSize = size;
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return // Container( // alignment: Alignment(x, y), child:
+    @override
+    void onScreenSizeChange(Vector2 size) {
+      final gameSize = size;
+      final gx = gameSize[0];
+      final gy = gameSize[1];
+      final diagonal = sqrt(gx * gx + gy * gy);
+      final stepLength = diagonal / defaultBallFPS * speed / 1000;
+      stepInterval = diagonal / stepLength;
+      _stepX = stepLength * dx;
+      _stepY = stepLength * dy;
+      logger.finer("stepInterval = $stepInterval");
+      final oSize = calcSize(gameSize, ratio);
+      iSize = oSize * iRatio;
+      logger.finer("Ball outer size = $oSize");
+      this.size = Vector2.all(oSize);
+      alignment = GameObjectAlignment.center;
+      final double selfPaddleYPos = selfPaddle.position[1];
+      final double selfPaddleHSize = selfPaddle.size[1];
+      final double selfPaddleTopSurface = selfPaddleYPos - selfPaddleHSize / 2;
+      position =
+          Vector2(gx / 2, selfPaddleTopSurface - oSize / 2 - gap); // , gy / 2);
+      initialised = true;
+      _stepCount = 0;
+      randSignIterator.moveNext();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return // Container( // alignment: Alignment(x, y), child:
         Stack(children: [
-      Align(
-        alignment: Alignment.center,
-        child: Container(
-          decoration: const BoxDecoration(shape: shape, color: color),
-        ),
-      ),
-      Align(
-        alignment: coreAlignment,
-        child: Container(
-          decoration: const BoxDecoration(shape: shape, color: Colors.black),
-          width: iSize,
-          height: iSize,
-        ),
-      ),
-    ]);
-  }
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              decoration: const BoxDecoration(shape: shape, color: color),
+            ),
+          ),
+          Align(
+            alignment: coreAlignment,
+            child: Container(
+              decoration: const BoxDecoration(shape: shape, color: Colors.black),
+              width: iSize,
+              height: iSize,
+            ),
+          ),
+        ]);
+    }
 
-  @override
-  void onCollision(List<Collision> collisions) {
-    logger.info("Ball collided with ${collisions.length} collisions.");
-    for (Collision col in collisions) {
-      logger.finer("collision component is ${col.component}.");
-      if (col.component is PaddleO) {
-        resetYield();
-        final paddle = col.component as PaddleO;
-        if (!bounceAtPaddle(paddle, col.intersectionRect)) {
-          logger.fine("Paddle hit fail. Pausing..");
-          pause(paddle.pos);
+    @override
+    void onCollision(List<Collision> collisions) {
+      logger.info("Ball collided with ${collisions.length} collisions.");
+      for (Collision col in collisions) {
+        logger.finer("collision component is ${col.component}.");
+        if (col.component is PaddleO) {
+          resetYield();
+          final paddle = col.component as PaddleO;
+          if (!bounceAtPaddle(paddle, col.intersectionRect)) {
+            logger.fine("Paddle hit fail. Pausing..");
+            pause(paddle.pos);
+          }
+        }
+        else if (col.component is WallO) {
+          bounceAtWall(col.component as WallO);
+          logger.finer("bounce at wall.");
         }
       }
-      else if (col.component is WallO) {
-        bounceAtWall(col.component as WallO);
-        logger.finer("bounce at wall.");
-      }
     }
-  }
 
-  void resetYield() {
-    _yieldCount = 0;
-    yieldBallPos(null);
-    _pickupDeltaPositionQueue.clear();
-    logger.finer("yieldBallPos(null) to clear DPQueue.");
-  }
+    void resetYield() {
+      _yieldCount = 0;
+      yieldBallPos(null);
+      _pickupDeltaPositionQueue.clear();
+      logger.finer("yieldBallPos(null) to clear DPQueue.");
+    }
 
-  final int pickupDelay; // final Vector2 ballPos;;
-  final _pickupDeltaPositionQueue = Queue<DeltaPosition>();
-  final int pickupCycle;
-  int _lastUpdate = 0;
-  int _yieldCount = 0;
-  static const yieldMax = 6;
+    final int pickupDelay; // final Vector2 ballPos;;
+    final _pickupDeltaPositionQueue = Queue<DeltaPosition>();
+    final int pickupCycle;
+    int _lastUpdate = 0;
+    int _yieldCount = 0;
+    static const yieldMax = 6;
 
-  DeltaPosition yieldPosition(Duration delta, Vector2 position) {
-    return DeltaPosition(delta, position);
-  }
+    DeltaPosition yieldPosition(Duration delta, Vector2 position) {
+      return DeltaPosition(delta, position);
+    }
 
-  final motionCycleRatio = 2;
-  double _motionCount = 0;
+    final motionCycleRatio = 2;
+    double _motionCount = 0;
 
-  @override
-  void update(Duration delta) {
-    if (delta.inMilliseconds - _lastUpdate > stepInterval) {
-      _lastUpdate = delta.inMilliseconds;
-      position = stepForward();
-      // setState(() {
+    @override
+    void update(Duration delta) {
+      if (delta.inMilliseconds - _lastUpdate > stepInterval) {
+        _lastUpdate = delta.inMilliseconds;
+        position = stepForward();
+        // setState(() {
         iPos = iPos + (_rotateCW ? 1 : -1);
         coreAlignment = coreAlignments[iPos % coreAlignments.length];
         logger.finer("coreAlignment is set as $coreAlignment by $iPos.");
-      // });
-      rebuildWidget();
-      logger.finer("rebuild ball.");
-      logger.finest("Update ball pos: (${position[0]}, ${position[1]}).");
-      if (_stepCount % pickupCycle == 0) {
-        // delta != Duration.zero && position != Vector2.zero() &&
-        _pickupDeltaPositionQueue.add(DeltaPosition(delta, position));
-        if (_yieldCount < yieldMax && _pickupDeltaPositionQueue.length >= pickupDelay) {
-          yieldBallPos(_pickupDeltaPositionQueue.removeFirst());
-          ++_yieldCount;
+        // });
+        rebuildWidget();
+        logger.finer("rebuild ball.");
+        logger.finest("Update ball pos: (${position[0]}, ${position[1]}).");
+        if (_stepCount % pickupCycle == 0) {
+          // delta != Duration.zero && position != Vector2.zero() &&
+          _pickupDeltaPositionQueue.add(DeltaPosition(delta, position));
+          if (_yieldCount < yieldMax && _pickupDeltaPositionQueue.length >= pickupDelay) {
+            yieldBallPos(_pickupDeltaPositionQueue.removeFirst());
+            ++_yieldCount;
+          }
         }
+        if (_stepCount % (pickupCycle * motionCycleRatio) == 0 && _pickupDeltaPositionQueue.isNotEmpty) {
+          final Vector2 lastPosition = _pickupDeltaPositionQueue.elementAt(0).position;
+          motionLine.givenPosition = lastPosition;
+          motionLine.givenSize = size;
+          logger.fine("Motionline");
+          _motionCount++;
+        }
+        ++_stepCount;
       }
-      if (_stepCount % (pickupCycle * motionCycleRatio) == 0 && _pickupDeltaPositionQueue.isNotEmpty) {
-        final Vector2 lastPosition = _pickupDeltaPositionQueue.elementAt(0).position;
-        motionLine.givenPosition = lastPosition;
-        motionLine.givenSize = size;
-        logger.fine("Motionline");
-        _motionCount++;
+    }
+
+    Vector2 _step() {
+      final x = position[0];
+      final y = position[1];
+      final np = Vector2(x + stepX, y + stepY);
+      return np;
+      // position[0] += stepX;
+      // position[1] += stepY;
+    }
+
+    Vector2 stepForward() {
+      lastPosForBackward = position;
+      return _step();
+    }
+
+    bool stepBackward() {
+      if (lastPosForBackward != null) {
+        position = lastPosForBackward as Vector2;
+        logger.finer("step backward by lastPosForBackward.");
+        return true;
       }
-      ++_stepCount;
+      else {
+        logger.warning("step backward failed 'cause lastPosForBackward == null.");
+        return false;
+      }
     }
-  }
 
-  Vector2 _step() {
-    final x = position[0];
-    final y = position[1];
-    final np = Vector2(x + stepX, y + stepY);
-    return np;
-    // position[0] += stepX;
-    // position[1] += stepY;
-  }
+    // void clearStepCount() { _stepCount = 0; }
+    // void updateLastPosWithPosition() { lastPos = position; }
 
-  Vector2 stepForward() {
-    lastPosForBackward = position;
-    return _step();
-  }
-
-  bool stepBackward() {
-    if (lastPosForBackward != null) {
-      position = lastPosForBackward as Vector2;
-      logger.finer("step backward by lastPosForBackward.");
-      return true;
+    void _reverseDx() {
+      _dx = - _dx;
+      logger.finer("dx reverse.");
     }
-    else {
-      logger.warning("step backward failed 'cause lastPosForBackward == null.");
-      return false;
+
+    void _reverseDy() {
+      _dy = - _dy;
+      logger.finer("dy reverse.");
     }
-  }
 
-  // void clearStepCount() { _stepCount = 0; }
-  // void updateLastPosWithPosition() { lastPos = position; }
-
-  void _reverseDx() {
-    _dx = - _dx;
-    logger.finer("dx reverse.");
-  }
-
-  void _reverseDy() {
-    _dy = - _dy;
-    logger.finer("dy reverse.");
-  }
-
-  void _reverseByWallPos(wallPos pos) {
-    switch (pos) {
-      case wallPos.left:
-      case wallPos.right: // if( offsets[0] == 0 && offsets[1] != 0) {
-        _reverseDx();
-        logger.finer("reverseByWallPos dx.");
-        break;
-      case wallPos.top:
-      case wallPos.bottom:
-        _reverseDy();
-        logger.finer("reverseByWallPos dy.");
-        break;
-      default:
-        throw Exception ( " No match condition. " );
+    void _reverseByWallPos(wallPos pos) {
+      switch (pos) {
+        case wallPos.left:
+        case wallPos.right: // if( offsets[0] == 0 && offsets[1] != 0) {
+          _reverseDx();
+          logger.finer("reverseByWallPos dx.");
+          break;
+        case wallPos.top:
+        case wallPos.bottom:
+          _reverseDy();
+          logger.finer("reverseByWallPos dy.");
+          break;
+        default:
+          throw Exception ( " No match condition. " );
+      }
     }
-  }
 
-  void bounceAtWall(WallBaseO wallBase) { // Vector2 offsets) {
-    if(!stepBackward()) {
-      throw Exception("stepBackward failed!");
-    }
-    _reverseByWallPos(wallBase.pos);
-    _rotate();
-  }
-
-  void _rotate() {
-    final Vector2 _rotated = _getRotated();
-    if (dx.sign == _rotated[0].sign && dy.sign == _rotated[1].sign) {
-      logger.finer("before dx and dy are rotated: ($dx, $dy)");
-      _dx = _rotated[0];
-      _dy = _rotated[1];
-      logger.finer("after dx and dy are rotated: ($dx, $dy)");
-    }
-    else {
-      logger.warning("ball rotation failed.");
-    }
-  }
-
-  Vector2 _getRotated() {
-    final a = rotation;
-    final rotator = Matrix2(cos(a), -sin(a), sin(a), cos(a));
-    final vector = Vector2(dx, dy);
-    vector.postmultiply(rotator);
-    return vector;
-  }
-
-  static const collisionGap = 2;
-  bool bounceAtPaddle(PaddleO paddle, Rect intersectionRect) {
-    // wallPos wp) {
-    // clearStepCount();
-    // updateLastPosWithPosition();
-    // stepBackward();
-    final double yDistToPaddle = (position - paddle.position)[1];
-    final rx = position[0];
-    if (rx >= intersectionRect.left && rx <= intersectionRect.right) {
-      logger.fine("Paddle top/bottom hit Ball.");
-      _reverseDy(); // _dy = -_dy; // reverse dy
-      final lap = intersectionRect.bottom - intersectionRect.top;
-      assert(lap > 0);
-      final dist = Vector2(0, yDistToPaddle < 0 ? lap + collisionGap : -lap - collisionGap);
-      position.add(dist);
+    void bounceAtWall(WallBaseO wallBase) { // Vector2 offsets) {
+      if(!stepBackward()) {
+        throw Exception("stepBackward failed!");
+      }
+      _reverseByWallPos(wallBase.pos);
       _rotate();
-      // _pickupDeltaPositionQueue.clear();
-      return true;
     }
-    else {
-      logger.fine("Paddle side hit Ball.");
-      // reverseDx();
-      return false;
+
+    void _rotate() {
+      final Vector2 _rotated = _getRotated();
+      if (dx.sign == _rotated[0].sign && dy.sign == _rotated[1].sign) {
+        logger.finer("before dx and dy are rotated: ($dx, $dy)");
+        _dx = _rotated[0];
+        _dy = _rotated[1];
+        logger.finer("after dx and dy are rotated: ($dx, $dy)");
+      }
+      else {
+        logger.warning("ball rotation failed.");
+      }
     }
-  }
 
-  double _speedRatio = 1;
-  void changeSliderValue(double speed) {
-    _speedRatio = speed;
-  }
-  double getSliderValue() => _speedRatio;
+    Vector2 _getRotated() {
+      final a = rotation;
+      final rotator = Matrix2(cos(a), -sin(a), sin(a), cos(a));
+      final vector = Vector2(dx, dy);
+      vector.postmultiply(rotator);
+      return vector;
+    }
 
-}
+    static const collisionGap = 2;
+    bool bounceAtPaddle(PaddleO paddle, Rect intersectionRect) {
+      // wallPos wp) {
+      // clearStepCount();
+      // updateLastPosWithPosition();
+      final rx = position[0];
+      if (rx >= intersectionRect.left && rx <= intersectionRect.right) {
+        logger.fine("Paddle top/bottom hit Ball.");
+        // logger.fine('stepBackward. before position = [$position].');
+        // final backwardResult = stepBackward();
+        // logger.fine('stepBackwardResult: $backwardResult. after position = [$position].');
+        _reverseDy(); // _dy = -_dy; // reverse dy
+        logger.fine("dy is reversed to $dy.");
+        final lap = intersectionRect.bottom - intersectionRect.top;
+        assert(lap > 0);
+        final double yDistToPaddle = (position - paddle.position)[1];
+        final dist = Vector2(0, yDistToPaddle > 0 ? lap + collisionGap : -lap - collisionGap);
+        logger.fine("going to add position($position): $dist");
+        position.add(dist);
+        logger.fine("position is moved to $position");
+        logger.fine("rotated before dx, dy = ($dx, $dy).");
+        _rotate();
+        logger.fine("rotated after dx, dy = ($dx, $dy).");
+        logger.fine("before steps: ($_stepX, $_stepY).");
+        _setSteps();
+        logger.fine("after steps: ($_stepX, $_stepY).");
+        // _pickupDeltaPositionQueue.clear();
+        return true;
+      }
+      else {
+        logger.fine("Paddle side hit Ball.");
+        // reverseDx();
+        return false;
+      }
+    }
+
+    double _speedRatio = 1;
+    void changeSliderValue(double speed) {
+      _speedRatio = speed;
+    }
+    double getSliderValue() => _speedRatio;
+  }
 
 class RandAngleIterator extends Iterable with Iterator {
   final int start;
