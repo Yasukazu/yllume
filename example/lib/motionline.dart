5b52a35a5ced1a1e32f66c0cba0782c79dede442
Yasukazu Makino <mkn@yskz.dev>
2022-07-31 07:59:28 +0900

    fix onScreenSizeChange.

--- example/lib/motionline.dart
+++ example/lib/motionline.dart
@@ -1,30 +1,16 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'dart:collection';
 import 'main.dart'; // logger
 import 'package:illume/illume.dart';
-import 'WallBase.dart';
-import 'Ball.dart';
-import 'pongPage.dart';
-import 'Backwardable.dart';
 
 class MotionLine extends GameObject {
-  final Vector2 givenSize;
-  final Vector2 givenPosition = Vector2(0, 0);
+  final double givenRatio;
   double opacity = 0.0;
-  // double get opacity => _opacity;
-  // void set opacity(double v) { _opacity = v;}
-  final int seq;
+  final int seq; // ID
 
-  MotionLine(this.seq, this.givenSize);
-
-  void setPosition(Vector2 other) {
-    givenPosition.setFrom(other);
-  }
+  MotionLine(this.seq, this.givenRatio);
 
   @override init() {
-    size = givenSize;
-    // position = givenPosition;
+    size *= givenRatio;
     alignment = GameObjectAlignment.center;
     collidable = false;
     rebuildWidgetIfNeeded = true;
@@ -42,15 +28,13 @@ class MotionLine extends GameObject {
   }
 
   @override void update(Duration delta) {
-    // size = givenSize;
-    // position = givenPosition;
-    // opacity = 0.6;
     rebuildWidget();
     logger.finer("MotionLine: size: $size, position: $position, opacity: $opacity.");
   }
 
   @override void onScreenSizeChange(Vector2 size) {
-    // opacity = 0.0;
+    this.size.setFrom(size);
+    this.size *= givenRatio;
   }
 
   void turnOn() {
